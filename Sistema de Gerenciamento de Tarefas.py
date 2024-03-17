f30c9430e2ea201a823f762555526c66e31f51ea
Henrique Azevedo <156683885+henriiqueaze@users.noreply.github.com>
2024-03-17 09:53:42 -0300

    Finalizando primeiro código
    Finalizando o primeiro protótipo do projeto.

--- Sistema de Gerenciamento de Tarefas.py
+++ Sistema de Gerenciamento de Tarefas.py
@@ -1,215 +1,157 @@
-import tkinter
-from tkinter import *
+import tkinter as tk
 from tkinter import messagebox
-from tkinter import ttk
-from time import sleep
 
-nome_tarefas = list()
-descricao_tarefas = list()
-
-
-def Titulo():
-    string = 'Sistema de Gerenciamento de Tarefas'
-    tamanho = len(string) + 6
-    print('=' * tamanho)
-    print(string.center(tamanho))
-    print('=' * tamanho)
-    sleep(1.5)
+# Lista para armazenar os nomes das tarefas
+nome_tarefas = []
+# Lista para armazenar as descrições das tarefas
+descricao_tarefas = []
 
 
-def menu():
-    print('[1] - Adicionar Tarefa\n[2] - Exibir Tarefas\n[3] - Marcar como Concluída\n'
-          '[4] - Excluir Tarefa\n[5] - Sair')
-    resposta = int(input('Digite sua opção: '))
+# Função para enviar uma nova tarefa
+def Enviar_Tarefa():
+    enviar_desc = escrever_desc.get()
+    enviar_nome = escrever_nome.get()
 
-    while resposta < 1 or resposta > 5:
-        print('\033[31mERRO!\033[m Resposta Inválida.')
-        sleep(1.5)
-        resposta = int(input('Digite sua opção: '))
-
-    return resposta
-
-
-def Mostrar_Tarefas():
-    for numerar, tarefa in enumerate(nome_tarefas):
-        print(f'[{numerar + 1}] - {tarefa}')
-
-
-def TarefaNaoAdicionada():
-    print('Nenhuma tarefa foi adicionada ainda.')
-    sleep(1.5)
-
-
-def AdicionarTarefa():
-    nome_tarefas.append(str(input('Digite sua Tarefa: ')))
-    descricao_tarefas.append(str(input('Digite uma descrição da sua tarefa: ')))
-    print('Tarefa Adicionada!')
-    sleep(1.5)
-
-
-def ExibirTarefas():
-    if not nome_tarefas:
-        TarefaNaoAdicionada()
-
+    # Verificar se ambos os campos foram preenchidos
+    if enviar_desc == '' or enviar_nome == '':
+        messagebox.showerror("Erro", "Por favor, insira um nome e uma descrição para a tarefa.")
+        escrever_nome.focus_force()
     else:
-        Mostrar_Tarefas()
-        ver_tarefas = str(input('Deseja ver a descrição de alguma tarefa? '))
+        # Limpar as entradas após o envio da tarefa
+        escrever_nome.delete(0, 'end')
+        escrever_desc.delete(0, 'end')
 
-        while ver_tarefas not in 'SsNn':
-            print('\033[31mERRO!\033[m Resposta Inválida.')
-            sleep(1.5)
-            ver_tarefas = str(input('Deseja ver a descrição de alguma tarefa? '))
+        # Adicionar a nova tarefa às listas
+        descricao_tarefas.append(enviar_desc)
+        nome_tarefas.append(enviar_nome)
 
-        if ver_tarefas in 'Ss':
-            resposta = int(input('Qual tarefa você deseja verificar? ')) - 1
-            print(descricao_tarefas[resposta])
-            sleep(3)
-
-        if ver_tarefas in 'Nn':
-            print('Carregando...')
-            sleep(1.5)
+        # Exibir mensagem de sucesso
+        messagebox.showinfo('Alerta!', 'Sua tarefa e descrição foram enviados!')
+        adicionar_tarefa_janela.destroy()
 
 
-def MarcarComoConcluida():
-    if not nome_tarefas:
-        TarefaNaoAdicionada()
+# Função para marcar uma tarefa como concluída
+def Enviar_Numero():
+    global marcar_como_concluida_numero
 
-    else:
-        Mostrar_Tarefas()
-        resposta = int(input('Qual tarefa você deseja marcar como concluída? ')) - 1
-        nome_tarefas[resposta] = ''.join(c + '\u0336' for c in nome_tarefas[resposta])
-        print('Tarefa Concluída!')
-        sleep(1.5)
+    # Obtendo o número inserido e convertendo para inteiro
+    marcar_numero_str = marcar_como_concluida_numero.get()
 
+    # Verificar se a string está vazia
+    if not marcar_numero_str:
+        messagebox.showerror("Erro", "Por favor, insira um número relacionado à tarefa.")
+        marcar_como_concluida_numero.focus_force()
+        return
 
-def ExcluirTarefa():
-    if not nome_tarefas:
-        TarefaNaoAdicionada()
+    try:
+        marcar_numero = int(marcar_numero_str) - 1
+    except ValueError:
+        messagebox.showerror("Erro", "Por favor, insira um número válido relacionado à tarefa.")
+        marcar_como_concluida_numero.focus_force()
+        return
 
-    else:
-        Mostrar_Tarefas()
-        resposta = int(input('Qual tarefa você deseja \033[31mexcluir\033[m? ')) - 1
-        nome_tarefas.pop(resposta)
-        descricao_tarefas.pop(resposta)
-        print('Tarefa excluída!')
-        sleep(1.5)
+    # Verificar se o número inserido é um índice válido
+    if marcar_numero < 0 or marcar_numero >= len(nome_tarefas):
+        messagebox.showerror("Erro", "O número inserido não corresponde a uma tarefa válida.")
+        marcar_como_concluida_numero.focus_force()
+        return
 
+    # Alterar o texto do item na lista de tarefas marcadas como concluídas
+    nome_tarefas[marcar_numero] = ''.join(c + '\u0336' for c in nome_tarefas[marcar_numero])
+    descricao_tarefas[marcar_numero] = ''.join(c + '\u0336' for c in descricao_tarefas[marcar_numero])
 
-def Sair():
-    resposta = str(input('Tem certeza que deseja sair? [\033[32mS\033[m/\033[31mN\033[m]: '))
-    while resposta not in 'SsNn':
-        print('\033[31mERRO!\033[m Resposta Inválida.')
-        sleep(1.5)
-        resposta = str(input('Tem certeza que deseja sair? [\033[32mS\033[m/\033[31mN\033[m]: '))
+    # Limpar a entrada do número
+    marcar_como_concluida_numero.delete(0, 'end')
 
-    if resposta in 'Ss':
-        print('Saindo...')
-        sleep(1.5)
-        print('Volte Sempre!')
-        exit()
-
-    if resposta in 'Nn':
-        print('Carregando...')
-        sleep(1.5)
+    # Exibir mensagem de sucesso
+    messagebox.showinfo('Alerta!', 'Sua tarefa foi marcada como concluída!')
+    marcar_como_concluida_janela.destroy()
 
 
-"""def programa():
-    Titulo()
+# Função para excluir uma tarefa
+def Excluir_Tarefa():
+    global excluir_numero
 
-    while True:
-        resposta_menu = menu()
-        if resposta_menu == 1:
-            AdicionarTarefa()
+    # Obtendo o número inserido e convertendo para inteiro
+    marcar_numero_str = excluir_numero.get()
 
-        elif resposta_menu == 2:
-            ExibirTarefas()
+    # Verificar se a string está vazia
+    if not marcar_numero_str:
+        messagebox.showerror("Erro", "Por favor, insira um número relacionado à tarefa.")
+        excluir_numero.focus_force()
+        return
 
-        elif resposta_menu == 3:
-            MarcarComoConcluida()
+    try:
+        marcar_numero = int(marcar_numero_str) - 1
+    except ValueError:
+        messagebox.showerror("Erro", "Por favor, insira um número válido relacionado à tarefa.")
+        excluir_numero.focus_force()
+        return
 
-        elif resposta_menu == 4:
-            ExcluirTarefa()
+    # Verificar se o número inserido é um índice válido
+    if marcar_numero < 0 or marcar_numero >= len(nome_tarefas):
+        messagebox.showerror("Erro", "O número inserido não corresponde a uma tarefa válida.")
+        excluir_numero.focus_force()
+        return
 
-        elif resposta_menu == 5:
-            Sair()
+    # Excluir a tarefa das listas
+    del (nome_tarefas[marcar_numero], descricao_tarefas[marcar_numero])
+
+    # Limpar a entrada do número
+    excluir_numero.delete(0, 'end')
+
+    # Exibir mensagem de sucesso
+    messagebox.showinfo('Alerta!', 'Sua tarefa foi excluída!')
+    excluir_tarefa_janela.destroy()
 
 
-programa()"""
+# Função para exibir uma mensagem quando nenhuma tarefa foi adicionada
+def Nenhuma_Tarefa_Adicionada(janela):
+    nenhuma_tarefa_adicionada_texto1 = tk.Label(janela, text='Nenhuma tarefa foi', font="Arial 19 bold",
+                                                bg="#262A40", fg="#F2F2F2")
+    nenhuma_tarefa_adicionada_texto2 = tk.Label(janela, text='adicionada ainda.', font="Arial 19 bold",
+                                                bg="#262A40", fg="#F2F2F2")
+    nenhuma_tarefa_adicionada_texto1.place(x=35, y=60)
+    nenhuma_tarefa_adicionada_texto2.place(x=43, y=100)
 
 
-# Primeiro Botão - Configurações
+# Função para abrir a janela de adicionar tarefa
 def JanelaAdicionarTarefa():
-    # Configurações da Janela
-    global adicionar_tarefa_janela
-    adicionar_tarefa_janela = Tk()
+    global adicionar_tarefa_janela, escrever_nome, escrever_desc
+
+    adicionar_tarefa_janela = tk.Toplevel(janela)
     adicionar_tarefa_janela.title('Adicionar Tarefa')
     adicionar_tarefa_janela.config(background="#262A40")
     adicionar_tarefa_janela.geometry('300x200')
     adicionar_tarefa_janela.resizable(False, False)
 
     # Adicionando o texto da janela
-    adicionar_tarefa_texto1 = Label(adicionar_tarefa_janela, text='Adicionar nome e', font="Arial 15 bold",
-                                    bg="#262A40", fg="#F2F2F2")
-    adicionar_tarefa_texto2 = Label(adicionar_tarefa_janela, text='descrição da tarefa', font="Arial 15 bold",
-                                    bg="#262A40", fg="#F2F2F2")
+    adicionar_tarefa_texto1 = tk.Label(adicionar_tarefa_janela, text='Adicionar nome e', font="Arial 15 bold",
+                                       bg="#262A40", fg="#F2F2F2")
+    adicionar_tarefa_texto2 = tk.Label(adicionar_tarefa_janela, text='descrição da tarefa', font="Arial 15 bold",
+                                       bg="#262A40", fg="#F2F2F2")
     adicionar_tarefa_texto1.place(x=64, y=10)
     adicionar_tarefa_texto2.place(x=55, y=35)
 
     # Escrever a primeira Entry
-    global escrever_nome
-    escrever_nome = Entry(adicionar_tarefa_janela, width=35)
+    escrever_nome = tk.Entry(adicionar_tarefa_janela, width=35)
     escrever_nome.place(x=45, y=80)
     escrever_nome.insert(0, 'Nome:')
 
     # Escrever a segunda Entry
-    global escrever_desc
-    escrever_desc = Entry(adicionar_tarefa_janela, width=35)
+    escrever_desc = tk.Entry(adicionar_tarefa_janela, width=35)
     escrever_desc.place(x=45, y=110)
     escrever_desc.insert(0, 'Descrição:')
 
     # Botão enviar
-    botao_enviar = Button(adicionar_tarefa_janela, text='Enviar', height=1, width=5, command=Enviar_Tarefa)
+    botao_enviar = tk.Button(adicionar_tarefa_janela, text='Enviar', height=1, width=5, command=Enviar_Tarefa)
     botao_enviar.place(x=125, y=150)
 
-    adicionar_tarefa_janela.mainloop()
 
-
-# Envio de Tarefas para as listas e fim da janela
-def Enviar_Tarefa():
-    enviar_desc = escrever_desc.get()
-    enviar_nome = escrever_nome.get()
-
-    if enviar_desc == '' or enviar_nome == '':
-        messagebox.showerror("Erro", "Por favor, insira um nome e uma descrição para a tarefa.")
-        escrever_nome.focus_force()
-
-    else:
-        escrever_nome.delete(0, 'end')
-        escrever_desc.delete(0, 'end')
-
-        # Enviando as respostas das Entrys para as listas
-        descricao_tarefas.append(enviar_desc)
-        nome_tarefas.append(enviar_nome)
-
-        # Alerta da tarefa e exlusão da imagem
-        messagebox.showinfo('Alerta!', 'Sua tarefa e descrição foram enviados!')
-        escrever_desc.focus_force()
-        adicionar_tarefa_janela.destroy()
-
-
-# Nenhuma tarefa adicionada função
-def Nenhuma_Tarefa_Adicionada(janela):
-    nenhuma_tarefa_adicionada_texto1 = Label(janela, text='Nenhuma tarefa foi', font="Arial 19 bold",
-                                             bg="#262A40", fg="#F2F2F2")
-    nenhuma_tarefa_adicionada_texto2 = Label(janela, text='adicionada ainda.', font="Arial 19 bold",
-                                             bg="#262A40", fg="#F2F2F2")
-    nenhuma_tarefa_adicionada_texto1.place(x=35, y=60)
-    nenhuma_tarefa_adicionada_texto2.place(x=43, y=100)
-
-
-# Segundo Botão - Configurações
+# Função para abrir a janela de exibir tarefas
 def JanelaExibirTarefa():
-    exibir_tarefa_janela = Tk()
+    global exibir_tarefa_janela
+    exibir_tarefa_janela = tk.Toplevel(janela)
     exibir_tarefa_janela.title('Exibir Tarefa')
     exibir_tarefa_janela.config(background="#262A40")
     exibir_tarefa_janela.geometry('300x200')
@@ -219,22 +161,28 @@ def JanelaExibirTarefa():
         Nenhuma_Tarefa_Adicionada(exibir_tarefa_janela)
 
     else:
-        nomes = Label(exibir_tarefa_janela, text='Tarefas: ')
-        descricao = Label(exibir_tarefa_janela, text='Descrição: ')
+        nomes = tk.Label(exibir_tarefa_janela, text='Tarefas:          Descrição:', bg='#262A40', fg='white',
+                         font='Arial 15 bold')
+        nomes.place(x=27, y=25)
 
-        canvas = tkinter.Canvas(exibir_tarefa_janela, width=400, height=400, bg="#262A40")
-        canvas.pack()
+        # Convertendo os elementos da lista para strings
+        quebra_linha1 = '\n'.join(str(tarefa) for tarefa in nome_tarefas)
 
-        canvas.create_line(200, 0, 200, 400, fill="black")
+        tarefas = tk.Label(exibir_tarefa_janela, text=quebra_linha1, bg='#262A40', fg='white',
+                           font='Arial 12 bold')
+        tarefas.place(x=38, y=63)
 
-        tarefas = tkinter.Label(exibir_tarefa_janela, text=nome_tarefas)
-        tarefas.place(x=75, y=100)
+        quebra_linha2 = '\n'.join(descricao_tarefas)
+        descricao = tk.Label(exibir_tarefa_janela, text=quebra_linha2, bg='#262A40', fg='white',
+                             font='Arial 12 bold')
+
+        descricao.place(x=175, y=63)
 
 
-
-# Terceiro Botão - Configurações
+# Função para abrir a janela de marcar tarefa como concluída
 def JanelaMarcarComoConcluida():
-    marcar_como_concluida_janela = Tk()
+    global marcar_como_concluida_numero, marcar_como_concluida_janela
+    marcar_como_concluida_janela = tk.Toplevel(janela)
     marcar_como_concluida_janela.title('Marcar Como Concluída')
     marcar_como_concluida_janela.config(background="#262A40")
     marcar_como_concluida_janela.geometry('300x200')
@@ -242,11 +190,33 @@ def JanelaMarcarComoConcluida():
 
     if not nome_tarefas and not descricao_tarefas:
         Nenhuma_Tarefa_Adicionada(marcar_como_concluida_janela)
+    else:
+        nomes = tk.Label(marcar_como_concluida_janela, text='Tarefas:          Descrição:', bg='#262A40', fg='white',
+                         font='Arial 15 bold')
+        nomes.place(x=27, y=25)
+        quebra_linha1 = '\n'.join(nome_tarefas)
+        tarefas = tk.Label(marcar_como_concluida_janela, text=quebra_linha1, bg='#262A40', fg='white',
+                           font='Arial 12 bold')
+        tarefas.place(x=38, y=63)
+
+        quebra_linha2 = '\n'.join(descricao_tarefas)
+        descricao = tk.Label(marcar_como_concluida_janela, text=quebra_linha2, bg='#262A40', fg='white',
+                             font='Arial 12 bold')
+        descricao.place(x=175, y=63)
+
+        marcar_como_concluida_numero = tk.Entry(marcar_como_concluida_janela, width=20)
+        marcar_como_concluida_numero.place(x=85, y=141)
+        marcar_como_concluida_numero.insert(0, 'Número da Tarefa:')
+
+        botao_enviar_numero = tk.Button(marcar_como_concluida_janela, text='Enviar', height=1, width=5,
+                                        command=Enviar_Numero)
+        botao_enviar_numero.place(x=120, y=161)
 
 
-# Quarto Botão - Configurações
+# Função para abrir a janela de excluir tarefa
 def JanelaExcluirTarefa():
-    excluir_tarefa_janela = Tk()
+    global excluir_numero, excluir_tarefa_janela
+    excluir_tarefa_janela = tk.Toplevel(janela)
     excluir_tarefa_janela.title('Excluir Tarefa')
     excluir_tarefa_janela.config(background="#262A40")
     excluir_tarefa_janela.geometry('300x200')
@@ -255,43 +225,65 @@ def JanelaExcluirTarefa():
     if not nome_tarefas and not descricao_tarefas:
         Nenhuma_Tarefa_Adicionada(excluir_tarefa_janela)
 
+    else:
+        nomes = tk.Label(excluir_tarefa_janela, text='Tarefas:          Descrição:', bg='#262A40',
+                         fg='white',
+                         font='Arial 15 bold')
+        nomes.place(x=27, y=25)
+        quebra_linha1 = '\n'.join(nome_tarefas)
+        tarefas = tk.Label(excluir_tarefa_janela, text=quebra_linha1, bg='#262A40', fg='white',
+                           font='Arial 12 bold')
+        tarefas.place(x=38, y=63)
 
-# Janela de Tarefas Configurações
-janela = Tk()
+        quebra_linha2 = '\n'.join(descricao_tarefas)
+        descricao = tk.Label(excluir_tarefa_janela, text=quebra_linha2, bg='#262A40', fg='white',
+                             font='Arial 12 bold')
+        descricao.place(x=175, y=63)
+
+        excluir_numero = tk.Entry(excluir_tarefa_janela, width=20)
+        excluir_numero.place(x=85, y=141)
+        excluir_numero.insert(0, 'Número da Tarefa:')
+
+        botao_excluir_numero = tk.Button(excluir_tarefa_janela, text='Enviar', height=1, width=5,
+                                         command=Excluir_Tarefa)
+        botao_excluir_numero.place(x=120, y=161)
+
+
+# Janela principal
+janela = tk.Tk()
 janela.title('Gerenciamento de Tarefas')
 janela.geometry('500x450')
 janela.config(background="#262A40")
 janela.resizable(False, False)
 
 # Texto Inicial
-titulo = Label(janela, text='Sistema de Gerenciamento', font="Arial 20 bold", bg="#262A40", fg="#F2F2F2")
+titulo = tk.Label(janela, text='Sistema de Gerenciamento', font="Arial 20 bold", bg="#262A40", fg="#F2F2F2")
 titulo.place(x=75, y=15)
 
-titulo1 = Label(janela, text='de Tarefas', font="Arial 20 bold", bg="#262A40", fg="#F2F2F2")
+titulo1 = tk.Label(janela, text='de Tarefas', font="Arial 20 bold", bg="#262A40", fg="#F2F2F2")
 titulo1.place(x=180, y=50)
 
-# Primeiro Botão
-adicionar_tarefa_botao = Button(janela, text="Adicionar Tarefa", bg="#416CA6", fg="#0E1521", activebackground="#85BFF2",
-                                activeforeground="black", font="Arial 12 bold", height=2, width=36,
-                                command=JanelaAdicionarTarefa)
+# Botões
+adicionar_tarefa_botao = tk.Button(janela, text="Adicionar Tarefa", bg="#416CA6", fg="#0E1521",
+                                   activebackground="#85BFF2",
+                                   activeforeground="black", font="Arial 12 bold", height=2, width=36,
+                                   command=JanelaAdicionarTarefa)
 adicionar_tarefa_botao.place(x=70, y=115)
 
-# Segundo Botão
-exibir_tarefa_botao = Button(janela, text='Exibir Tarefas', bg="#416CA6", fg="#0E1521", activebackground="#85BFF2",
-                             activeforeground="black", font="Arial 12 bold", height=2, width=36,
-                             command=JanelaExibirTarefa)
+exibir_tarefa_botao = tk.Button(janela, text='Exibir Tarefas', bg="#416CA6", fg="#0E1521", activebackground="#85BFF2",
+                                activeforeground="black", font="Arial 12 bold", height=2, width=36,
+                                command=JanelaExibirTarefa)
 exibir_tarefa_botao.place(x=70, y=195)
 
-# Terceiro Botão
-marcar_como_concluida_botao = Button(janela, text='Marcar Como Concluída', bg="#416CA6", fg="#0E1521",
-                                     font="Arial 12 bold", height=2, width=36, activebackground="#85BFF2",
-                                     activeforeground="black", command=JanelaMarcarComoConcluida)
+marcar_como_concluida_botao = tk.Button(janela, text='Marcar Como Concluída', bg="#416CA6", fg="#0E1521",
+                                        font="Arial 12 bold", height=2, width=36, activebackground="#85BFF2",
+                                        activeforeground="black", command=JanelaMarcarComoConcluida)
 marcar_como_concluida_botao.place(x=70, y=275)
 
-# Quarto Botão
-excluir_tarefa_botao = Button(janela, text='Excluir Tarefa', bg="#416CA6", fg="#202336", font="Arial 12 bold", height=2,
-                              width=36, activebackground="#85BFF2", activeforeground="black",
-                              command=JanelaExcluirTarefa)
+excluir_tarefa_botao = tk.Button(janela, text='Excluir Tarefa', bg="#416CA6", fg="#202336", font="Arial 12 bold",
+                                 height=2,
+                                 width=36, activebackground="#85BFF2", activeforeground="black",
+                                 command=JanelaExcluirTarefa)
 excluir_tarefa_botao.place(x=70, y=355)
 
 janela.mainloop()
