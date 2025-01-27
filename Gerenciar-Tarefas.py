e4ba5c427ec3fb64d74769c6afa6e9772dc6c5a1
Henrique Azevedo <henriquee.aze.dev@gmail.com>
2025-01-27 15:04:53 -0300

    Arumando tela de alerta

--- Gerenciar-Tarefas.py
+++ Gerenciar-Tarefas.py
@@ -1,198 +1,217 @@
 import tkinter as tk
 from tkinter import messagebox
 
-class TaskManager:
-    def __init__(self):
-        self.file_name = 'tarefas.txt'
-        self.tasks = []
-        self.load_tasks()
+class Tarefa:
+    def __init__(self, nome, descricao):
+        self.nome = nome
+        self.descricao = descricao
 
-    def load_tasks(self):
+    def marcar_concluida(self):
+        self.nome = ''.join(c + '\u0336' for c in self.nome)
+        self.descricao = ''.join(c + '\u0336' for c in self.descricao)
+
+def Arquivo_Existe(nome_arquivo):
+    try:
+        with open(nome_arquivo, 'r'):
+            return True
+    except FileNotFoundError:
+        return False
+
+def Criar_Arquivo(nome_arquivo):
+    try:
+        with open(nome_arquivo, 'w') as arquivo:
+            arquivo.write('')
+    except IOError:
+        print(f"Erro ao criar o arquivo '{nome_arquivo}'.")
+
+class GerenciadorTarefas:
+    def __init__(self, arquivo):
+        self.arquivo = arquivo
+        self.tarefas = []
+        if not Arquivo_Existe(arquivo):
+            Criar_Arquivo(arquivo)
+        self.carregar_tarefas()
+
+    def carregar_tarefas(self):
         try:
-            with open(self.file_name, 'r') as file:
-                for line in file:
-                    name, description = line.strip().split('|')
-                    self.tasks.append(Task(name, description))
+            with open(self.arquivo, 'r') as arquivo_tarefas:
+                for linha in arquivo_tarefas:
+                    nome, descricao = linha.strip().split('|')
+                    self.tarefas.append(Tarefa(nome, descricao))
         except FileNotFoundError:
-            with open(self.file_name, 'w'):
-                pass
+            print(f"O arquivo '{self.arquivo}' não foi encontrado.")
 
-    def save_tasks(self):
+    def salvar_tarefas(self):
         try:
-            with open(self.file_name, 'w') as file:
-                for task in self.tasks:
-                    file.write(f"{task.name}|{task.description}\n")
+            with open(self.arquivo, 'w') as arquivo_tarefas:
+                for tarefa in self.tarefas:
+                    arquivo_tarefas.write(f"{tarefa.nome}|{tarefa.descricao}\n")
         except IOError:
-            print(f"Erro ao salvar as tarefas no arquivo '{self.file_name}'.")
+            print(f"Erro ao salvar as tarefas no arquivo '{self.arquivo}'.")
 
-    def add_task(self, name, description):
-        self.tasks.append(Task(name, description))
-        self.save_tasks()
+    def adicionar_tarefa(self, nome, descricao):
+        self.tarefas.append(Tarefa(nome, descricao))
+        self.salvar_tarefas()
 
-    def mark_task_completed(self, index):
-        if 0 <= index < len(self.tasks):
-            self.tasks[index].mark_completed()
-            self.save_tasks()
+    def excluir_tarefa(self, indice):
+        if 0 <= indice < len(self.tarefas):
+            del self.tarefas[indice]
+            self.salvar_tarefas()
 
-    def delete_task(self, index):
-        if 0 <= index < len(self.tasks):
-            del self.tasks[index]
-            self.save_tasks()
+    def marcar_concluida(self, indice):
+        if 0 <= indice < len(self.tarefas):
+            self.tarefas[indice].marcar_concluida()
+            self.salvar_tarefas()
 
+class InterfaceTarefa:
+    def __init__(self, gerenciador):
+        self.gerenciador = gerenciador
+        self.janela = tk.Tk()
+        self.janela.title('Gerenciamento de Tarefas')
+        self.janela.geometry('500x450')
+        self.janela.config(background="#262A40")
+        self.janela.resizable(False, False)
+        self.configurar_interface()
+        self.janela.mainloop()
 
-class Task:
-    def __init__(self, name, description):
-        self.name = name
-        self.description = description
-        self.completed = False
+    def configurar_interface(self):
+        titulo = tk.Label(self.janela, text='Sistema de Gerenciamento', font="Arial 20 bold", bg="#262A40", fg="#F2F2F2")
+        titulo.place(x=75, y=15)
+        titulo1 = tk.Label(self.janela, text='de Tarefas', font="Arial 20 bold", bg="#262A40", fg="#F2F2F2")
+        titulo1.place(x=180, y=50)
 
-    def mark_completed(self):
-        self.name = ''.join(c + '\u0336' for c in self.name)
-        self.description = ''.join(c + '\u0336' for c in self.description)
-        self.completed = True
+        botao_adicionar = tk.Button(self.janela, text="Adicionar Tarefa", bg="#416CA6", fg="#0E1521", activebackground="#85BFF2",
+                                     activeforeground="black", font="Arial 12 bold", height=2, width=36, command=self.janela_adicionar_tarefa)
+        botao_adicionar.place(x=70, y=115)
 
+        botao_exibir = tk.Button(self.janela, text='Exibir Tarefas', bg="#416CA6", fg="#0E1521", activebackground="#85BFF2",
+                                  activeforeground="black", font="Arial 12 bold", height=2, width=36, command=self.janela_exibir_tarefa)
+        botao_exibir.place(x=70, y=195)
 
-class TaskApp:
-    def __init__(self, root):
-        self.manager = TaskManager()
-        self.root = root
-        self.root.title('Gerenciamento de Tarefas')
-        self.root.geometry('500x450')
-        self.root.config(background="#262A40")
-        self.root.resizable(False, False)
+        botao_marcar = tk.Button(self.janela, text='Marcar Como Concluída', bg="#416CA6", fg="#0E1521",
+                                  font="Arial 12 bold", height=2, width=36, activebackground="#85BFF2",
+                                  activeforeground="black", command=self.janela_marcar_concluida)
+        botao_marcar.place(x=70, y=275)
 
-        self.create_widgets()
+        botao_excluir = tk.Button(self.janela, text='Excluir Tarefa', bg="#416CA6", fg="#0E1521", font="Arial 12 bold",
+                                   height=2, width=36, activebackground="#85BFF2", activeforeground="black",
+                                   command=self.janela_excluir_tarefa)
+        botao_excluir.place(x=70, y=355)
 
-    def create_widgets(self):
-        tk.Label(self.root, text='Sistema de Gerenciamento', font="Arial 20 bold", bg="#262A40", fg="#F2F2F2").place(x=75, y=15)
-        tk.Label(self.root, text='de Tarefas', font="Arial 20 bold", bg="#262A40", fg="#F2F2F2").place(x=180, y=50)
+    def janela_adicionar_tarefa(self):
+        janela = tk.Toplevel(self.janela)
+        janela.title('Adicionar Tarefa')
+        janela.config(background="#262A40")
+        janela.geometry('300x200')
+        janela.resizable(False, False)
 
-        tk.Button(self.root, text="Adicionar Tarefa", bg="#416CA6", fg="#0E1521", activebackground="#85BFF2",
-                  activeforeground="black", font="Arial 12 bold", height=2, width=36, command=self.open_add_task_window).place(x=70, y=115)
+        tk.Label(janela, text='Adicionar nome e', font="Arial 15 bold", bg="#262A40", fg="#F2F2F2").place(x=64, y=10)
+        tk.Label(janela, text='descrição da tarefa', font="Arial 15 bold", bg="#262A40", fg="#F2F2F2").place(x=55, y=35)
 
-        tk.Button(self.root, text='Exibir Tarefas', bg="#416CA6", fg="#0E1521", activebackground="#85BFF2",
-                  activeforeground="black", font="Arial 12 bold", height=2, width=36, command=self.open_view_tasks_window).place(x=70, y=195)
+        nome_entry = tk.Entry(janela, width=35)
+        nome_entry.place(x=45, y=80)
+        nome_entry.insert(0, 'Nome:')
 
-        tk.Button(self.root, text='Marcar Como Concluída', bg="#416CA6", fg="#0E1521",
-                  font="Arial 12 bold", height=2, width=36, activebackground="#85BFF2",
-                  activeforeground="black", command=self.open_mark_completed_window).place(x=70, y=275)
+        descricao_entry = tk.Entry(janela, width=35)
+        descricao_entry.place(x=45, y=110)
+        descricao_entry.insert(0, 'Descrição:')
 
-        tk.Button(self.root, text='Excluir Tarefa', bg="#416CA6", fg="#0E1521", font="Arial 12 bold",
-                  height=2, width=36, activebackground="#85BFF2", activeforeground="black",
-                  command=self.open_delete_task_window).place(x=70, y=355)
-
-    def open_add_task_window(self):
-        add_task_window = tk.Toplevel(self.root)
-        add_task_window.title('Adicionar Tarefa')
-        add_task_window.geometry('300x200')
-        add_task_window.config(background="#262A40")
-
-        tk.Label(add_task_window, text='Adicionar nome e', font="Arial 15 bold", bg="#262A40", fg="#F2F2F2").place(x=64, y=10)
-        tk.Label(add_task_window, text='descrição da tarefa', font="Arial 15 bold", bg="#262A40", fg="#F2F2F2").place(x=55, y=35)
-
-        name_entry = tk.Entry(add_task_window, width=35)
-        name_entry.place(x=45, y=80)
-        name_entry.insert(0, 'Nome:')
-
-        desc_entry = tk.Entry(add_task_window, width=35)
-        desc_entry.place(x=45, y=110)
-        desc_entry.insert(0, 'Descrição:')
-
-        def submit_task():
-            name = name_entry.get()
-            description = desc_entry.get()
-            if name and description:
-                self.manager.add_task(name, description)
-                messagebox.showinfo('Alerta!', 'Sua tarefa e descrição foram enviados!')
-                add_task_window.destroy()
-            else:
+        def enviar():
+            nome = nome_entry.get()
+            descricao = descricao_entry.get()
+            if not nome or not descricao:
                 messagebox.showerror("Erro", "Por favor, insira um nome e uma descrição para a tarefa.")
+                return
+            self.gerenciador.adicionar_tarefa(nome, descricao)
+            messagebox.showinfo('Alerta!', 'Sua tarefa e descrição foram enviados!')
+            janela.destroy()
 
-        tk.Button(add_task_window, text='Enviar', height=1, width=5, command=submit_task).place(x=125, y=150)
+        tk.Button(janela, text='Enviar', height=1, width=5, command=enviar).place(x=125, y=150)
 
-    def open_view_tasks_window(self):
-        view_tasks_window = tk.Toplevel(self.root)
-        view_tasks_window.title('Exibir Tarefas')
-        view_tasks_window.geometry('300x200')
-        view_tasks_window.config(background="#262A40")
+    def janela_exibir_tarefa(self):
+        if not self.gerenciador.tarefas:
+            messagebox.showinfo('Alerta!', 'Nenhuma tarefa foi adicionada.')
+            return
 
-        if not self.manager.tasks:
-            tk.Label(view_tasks_window, text='Nenhuma tarefa foi adicionada ainda.', font="Arial 19 bold",
-                     bg="#262A40", fg="#F2F2F2").pack(pady=80)
-        else:
-            tk.Label(view_tasks_window, text='Tarefas:          Descrição:', bg='#262A40', fg='white',
-                     font='Arial 15 bold').place(x=27, y=25)
+        janela = tk.Toplevel(self.janela)
+        janela.title('Exibir Tarefa')
+        janela.config(background="#262A40")
+        janela.geometry('300x200')
+        janela.resizable(False, False)
 
-            tasks_text = '\n'.join([task.name for task in self.manager.tasks])
-            desc_text = '\n'.join([task.description for task in self.manager.tasks])
+        tk.Label(janela, text='Tarefas:          Descrição:', bg='#262A40', fg='white', font='Arial 15 bold').place(x=27, y=25)
+        tarefas = '\n'.join(f"{i+1}. {tarefa.nome}" for i, tarefa in enumerate(self.gerenciador.tarefas))
+        descricoes = '\n'.join(f"{tarefa.descricao}" for tarefa in self.gerenciador.tarefas)
 
-            tk.Label(view_tasks_window, text=tasks_text, bg='#262A40', fg='white', font='Arial 12 bold').place(x=38, y=63)
-            tk.Label(view_tasks_window, text=desc_text, bg='#262A40', fg='white', font='Arial 12 bold').place(x=175, y=63)
+        tk.Label(janela, text=tarefas, bg='#262A40', fg='white', font='Arial 12 bold').place(x=38, y=63)
+        tk.Label(janela, text=descricoes, bg='#262A40', fg='white', font='Arial 12 bold').place(x=175, y=63)
 
-    def open_mark_completed_window(self):
-        mark_completed_window = tk.Toplevel(self.root)
-        mark_completed_window.title('Marcar Como Concluída')
-        mark_completed_window.geometry('300x200')
-        mark_completed_window.config(background="#262A40")
+    def janela_marcar_concluida(self):
+        if not self.gerenciador.tarefas:
+            messagebox.showinfo('Alerta!', 'Nenhuma tarefa foi adicionada.')
+            return
 
-        if not self.manager.tasks:
-            tk.Label(mark_completed_window, text='Nenhuma tarefa foi adicionada ainda.', font="Arial 19 bold",
-                     bg="#262A40", fg="#F2F2F2").pack(pady=80)
-        else:
-            tk.Label(mark_completed_window, text='Tarefas:          Descrição:', bg='#262A40', fg='white',
-                     font='Arial 15 bold').place(x=27, y=25)
+        janela = tk.Toplevel(self.janela)
+        janela.title('Marcar Como Concluída')
+        janela.config(background="#262A40")
+        janela.geometry('300x200')
+        janela.resizable(False, False)
 
-            tasks_text = '\n'.join([task.name for task in self.manager.tasks])
-            tk.Label(mark_completed_window, text=tasks_text, bg='#262A40', fg='white', font='Arial 12 bold').place(x=38, y=63)
+        tk.Label(janela, text='Tarefas:          Descrição:', bg='#262A40', fg='white', font='Arial 15 bold').place(x=27, y=25)
+        tarefas = '\n'.join(f"{i+1}. {tarefa.nome}" for i, tarefa in enumerate(self.gerenciador.tarefas))
+        descricoes = '\n'.join(f"{tarefa.descricao}" for tarefa in self.gerenciador.tarefas)
 
-            entry = tk.Entry(mark_completed_window, width=20)
-            entry.place(x=85, y=141)
-            entry.insert(0, 'Número da Tarefa:')
+        tk.Label(janela, text=tarefas, bg='#262A40', fg='white', font='Arial 12 bold').place(x=38, y=63)
+        tk.Label(janela, text=descricoes, bg='#262A40', fg='white', font='Arial 12 bold').place(x=175, y=63)
 
-            def submit_completion():
-                try:
-                    index = int(entry.get()) - 1
-                    self.manager.mark_task_completed(index)
-                    messagebox.showinfo('Alerta!', 'Sua tarefa foi marcada como concluída!')
-                    mark_completed_window.destroy()
-                except (ValueError, IndexError):
-                    messagebox.showerror("Erro", "Por favor, insira um número válido relacionado à tarefa.")
+        numero_entry = tk.Entry(janela, width=20)
+        numero_entry.place(x=85, y=141)
+        numero_entry.insert(0, 'Número da Tarefa:')
 
-            tk.Button(mark_completed_window, text='Enviar', height=1, width=5, command=submit_completion).place(x=120, y=161)
+        def enviar():
+            try:
+                indice = int(numero_entry.get()) 
+                self.gerenciador.marcar_concluida(indice)
+                messagebox.showinfo('Alerta!', 'Sua tarefa foi marcada como concluída!')
+                janela.destroy()
+            except (ValueError, IndexError):
+                messagebox.showerror('Erro', 'Por favor, insira um número válido relacionado à tarefa.')
 
-    def open_delete_task_window(self):
-        delete_task_window = tk.Toplevel(self.root)
-        delete_task_window.title('Excluir Tarefa')
-        delete_task_window.geometry('300x200')
-        delete_task_window.config(background="#262A40")
+        tk.Button(janela, text='Enviar', height=1, width=5, command=enviar).place(x=120, y=161)
 
-        if not self.manager.tasks:
-            tk.Label(delete_task_window, text='Nenhuma tarefa foi adicionada ainda.', font="Arial 19 bold",
-                     bg="#262A40", fg="#F2F2F2").pack(pady=80)
-        else:
-            tk.Label(delete_task_window, text='Tarefas:          Descrição:', bg='#262A40', fg='white',
-                     font='Arial 15 bold').place(x=27, y=25)
+    def janela_excluir_tarefa(self):
+        if not self.gerenciador.tarefas:
+            messagebox.showinfo('Alerta!', 'Nenhuma tarefa foi adicionada.')
+            return
 
-            tasks_text = '\n'.join([task.name for task in self.manager.tasks])
-            tk.Label(delete_task_window, text=tasks_text, bg='#262A40', fg='white', font='Arial 12 bold').place(x=38, y=63)
+        janela = tk.Toplevel(self.janela)
+        janela.title('Excluir Tarefa')
+        janela.config(background="#262A40")
+        janela.geometry('300x200')
+        janela.resizable(False, False)
 
-            entry = tk.Entry(delete_task_window, width=20)
-            entry.place(x=85, y=141)
-            entry.insert(0, 'Número da Tarefa:')
+        tk.Label(janela, text='Tarefas:          Descrição:', bg='#262A40', fg='white', font='Arial 15 bold').place(x=27, y=25)
+        tarefas = '\n'.join(f"{i+1}. {tarefa.nome}" for i, tarefa in enumerate(self.gerenciador.tarefas))
+        descricoes = '\n'.join(f"{tarefa.descricao}" for tarefa in self.gerenciador.tarefas)
 
-            def submit_deletion():
-                try:
-                    index = int(entry.get()) - 1
-                    self.manager.delete_task(index)
-                    messagebox.showinfo('Alerta!', 'Sua tarefa foi excluída!')
-                    delete_task_window.destroy()
-                except (ValueError, IndexError):
-                    messagebox.showerror("Erro", "Por favor, insira um número válido relacionado à tarefa.")
+        tk.Label(janela, text=tarefas, bg='#262A40', fg='white', font='Arial 12 bold').place(x=38, y=63)
+        tk.Label(janela, text=descricoes, bg='#262A40', fg='white', font='Arial 12 bold').place(x=175, y=63)
 
-            tk.Button(delete_task_window, text='Enviar', height=1, width=5, command=submit_deletion).place(x=120, y=161)
+        numero_entry = tk.Entry(janela, width=20)
+        numero_entry.place(x=85, y=141)
+        numero_entry.insert(0, 'Número da Tarefa:')
 
+        def enviar():
+            try:
+                indice = int(numero_entry.get()) - 1
+                self.gerenciador.excluir_tarefa(indice)
+                messagebox.showinfo('Alerta!', 'Sua tarefa foi excluída com sucesso!')
+                janela.destroy()
+            except (ValueError, IndexError):
+                messagebox.showerror('Erro', 'Por favor, insira um número válido relacionado à tarefa.')
+
+        tk.Button(janela, text='Enviar', height=1, width=5, command=enviar).place(x=120, y=161)
 
 if __name__ == "__main__":
-    root = tk.Tk()
-    app = TaskApp(root)
-    root.mainloop()
+    gerenciador = GerenciadorTarefas("assets/Tarefas/Tarefas.txt")
+    InterfaceTarefa(gerenciador)
